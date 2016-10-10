c4e7f2f4e055dd2e5d107f6745b4ebac3ef45dbf
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2016-10-10 14:52:28 -0400

    Update radio channel.
    Closes #1

--- assets/js/submit-response.js
+++ assets/js/submit-response.js
@@ -1,7 +1,7 @@
 var submitResponseController = Marionette.Object.extend({
 
     initialize: function() {
-        this.listenTo( Backbone.Radio.channel( 'submit' ), 'submit:response', this.submitResponse );
+        this.listenTo( Backbone.Radio.channel( 'forms' ), 'submit:response', this.submitResponse );
     },
 
     submitResponse: function( response, textStatus, jqXHR ) {
